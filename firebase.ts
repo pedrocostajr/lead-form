2ad3cdbf0bad6e34561e25db538b5b8a1f43c77f
Pedro Adair Boeira Costa Junior <pedrocostajr@MacBook-Air-de-Pedro.local>
2026-02-24 01:54:03 -0300

    fix: hardcode firebase credentials for easier deployment

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyAs5N4-_iA8PL8XmcaDu2VJxU9wjrbywdY",
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "lead-form-4213b.firebaseapp.com",
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "lead-form-4213b",
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "lead-form-4213b.firebasestorage.app",
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "111852027687",
-    appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:111852027687:web:115b5acfabdd4ce5693302"
+    apiKey: "AIzaSyAs5N4-_iA8PL8XmcaDu2VJxU9wjrbywdY",
+    authDomain: "lead-form-4213b.firebaseapp.com",
+    projectId: "lead-form-4213b",
+    storageBucket: "lead-form-4213b.firebasestorage.app",
+    messagingSenderId: "111852027687",
+    appId: "1:111852027687:web:115b5acfabdd4ce5693302"
 };
 
 // Initialize Firebase
